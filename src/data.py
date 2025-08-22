e91fe72632e02f232a8246017c617b03b4f3e2a8
KristaDeimos <kristavoitishkina@yandex.ru>
2025-08-22 23:01:37 +0300

    Create data.py

---
+++ src/data.py
@@ -0,0 +1,63 @@
+service_url = 'https://qa-scooter.praktikum-services.ru'
+
+create_courier_url = service_url + '/api/v1/courier'
+login_courier_url = service_url + '/api/v1/courier/login'
+delete_courier_url = service_url + '/api/v1/courier/'
+
+CREATE_COURIER_DUPLICATION_ERROR_MESSAGE = 'Этот логин уже используется. Попробуйте другой.'
+CREATE_COURIER_EMPTY_FIELD_ERROR_MESSAGE = 'Недостаточно данных для создания учетной записи'
+LOGIN_WITH_INCORRECT_DATA_ERROR_MESSAGE = 'Учетная запись не найдена'
+LOGIN_WITH_EMPTY_FIELD_ERROR_MESSAGE = 'Недостаточно данных для входа'
+DELETE_WITH_EMPTY_ID_ERROR_MESSAGE = 'Недостаточно данных для удаления курьера'
+DELETE_WITH_UNEXISTING_ID_ERROR_MESSAGE = 'Курьера с таким id нет.'
+
+create_order_url = service_url + '/api/v1/orders'
+get_orders_url = service_url + '/api/v1/orders'
+
+ORDER_INFO_NO_COLOR = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-10-05",
+    "comment": "Saske, come back to Konoha",
+    "color": []
+    }
+
+ORDER_INFO_COLOR_BLACK = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-10-05",
+    "comment": "Saske, come back to Konoha",
+    "color": ["BLACK"]
+    }
+
+ORDER_INFO_COLOR_GREY = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-10-05",
+    "comment": "Saske, come back to Konoha",
+    "color": ["GRAY"]
+    }
+
+ORDER_INFO_COLOR_BLACK_AND_GREY = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-10-05",
+    "comment": "Saske, come back to Konoha",
+    "color": ["BLACK", "GREY"]
+    }
